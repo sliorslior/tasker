5cb0d919ba310557e97189c08806310a67702104
Lior Sagi <sliorslior@gmail.com>
2024-08-04 15:56:16 +0300

    task: implemented basic routes with no logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from app.config import Config
+from app.routes.task import TriggerTask, TaskStatus
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    api = Api(app)
+    api.add_resource(TriggerTask, '/tasks/run')
+    api.add_resource(TaskStatus, '/tasks/status')
 
     return app
